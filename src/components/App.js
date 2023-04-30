ade128efaa5637d15796f5f465faad00aab8b94a
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-04-30 20:41:22 +0300

    completed labs

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => setImages(data));
+  }, []);
+  return (
+    <>
+      {images.map((img) => {
+        if (img === null) {
+          return <p>Loading...</p>;
+        } else <img src={img} />;
+      })}
+    </>
+  );
+}
+export default App;
